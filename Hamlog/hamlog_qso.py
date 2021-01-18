08e083ce7c6218ac463edd685a4d4480e8a82e41
Kirill Kotyagin <k.kotyagin@wwpass.com>
2021-01-18 13:16:31 +0300

    ADD: qso dataclass prototype (to be finished)

---
+++ Hamlog/hamlog_qso.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, asdict
+from datetime import datetime
+from json import dumps as json_dumps
+
+@dataclass
+class HamlogQSO():
+
+    mycall      : str
+    call        : str
+    band        : str
+    mode        : str
+    timestamp   : datetime
+    snt         : str
+    rcv         : str
+
+    contestname     : str = None
+    contestnr       : str = None
+    rxfreq          : str = None
+    txfreq          : str = None
+    operator        : str = None
+    countryprefix   : str = None
+    wpxprefix       : str = None
+    stationprefix   : str = None
+    continent       : str = None
+    sntnr           : int = None
+    rcvnr           : str = None
+    gridsquare      : str = None
+    exchangel       : str = None
+    section         : str = None
+    comment         : str = None
+    qth             : str = None
+    name            : str = None
+    power           : str = None
+    misctext        : str = None
+    zone            : str = None
