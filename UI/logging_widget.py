a64854f5c65d5f3729521629f12770ef64778c8d
Kirill Kotyagin <kirill.kotyagin@gmail.com>
2021-01-25 10:14:01 +0300

    FIX: make logging thread sage (fixes crash on nt)

--- UI/logging_widget.py
+++ UI/logging_widget.py
@@ -1,23 +1,29 @@
 from logging import Handler as LoggingHandler, Formatter as LogFormatter, getLogger
-
+from PySide2.QtCore import Signal as QtSignal, QObject
 from PySide2.QtWidgets import QWidget, QPlainTextEdit, QVBoxLayout, QGroupBox
 
 from constants import APPLICATION_LOG_FORMAT, APPLICATION_LOG_LEVEL
 
 class PlainTextLoggingHandler(LoggingHandler):
-    def __init__(self, parent):
+
+    def __init__(self, _append_log_signal):
         super().__init__()
-        self.widget = QPlainTextEdit(parent)
-        self.widget.setReadOnly(True)    
+        self._log_widget = QPlainTextEdit()
+        self._log_widget.setReadOnly(True)
+        self._append_log_signal = _append_log_signal
+        self._append_log_signal.connect(self._log_widget.appendPlainText)
     
     def emit(self, record):
         msg = self.format(record)
-        self.widget.appendPlainText(msg)
+        self._append_log_signal.emit(msg)
 
     def get_widget(self):
-        return self.widget
+        return self._log_widget
 
 class LoggingWidget(QWidget):
+
+    _append_log_signal = QtSignal(str)
+
     def __init__(self):
         super().__init__()
         self.create_ui()
@@ -29,7 +35,7 @@ class LoggingWidget(QWidget):
         layout.addWidget(group_box)
         group_box_layout = QVBoxLayout()
         group_box.setLayout(group_box_layout)
-        logging_handler = PlainTextLoggingHandler(self)
+        logging_handler = PlainTextLoggingHandler(self._append_log_signal)
         logging_handler.setFormatter(LogFormatter(APPLICATION_LOG_FORMAT))
         logger = getLogger()
         logger.addHandler(logging_handler)
