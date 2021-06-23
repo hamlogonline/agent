33b731312f2dbfbcc141b90b1f1812751a936cd0
Kirill Kotyagin <kirill.kotyagin@gmail.com>
2021-06-23 16:56:58 +0300

    FIX: set version back to 0.0.7

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ from logging import INFO as APPLICATION_LOG_LEVEL
 APPLICATION_NAME = 'HAMLOG Agent'
 APPLICATION_ORGANIZATION_NAME = 'HAMLOG LLC'
 APPLICATION_ORGANIZATION_DOMAIN = 'com.hamlog.agent'
-APPLICATION_VERSION = '0.0.1'
+APPLICATION_VERSION = '0.0.7'
 
 APPLICATION_LOG_FILE_NAME = 'hamlog-agent-log.txt'
 APPLICATION_LOG_FORMAT = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
